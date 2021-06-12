a5a2e8a6b136b91c3bb1c2b317ec600450d5a902
soyeon207 <psy010207@naver.com>
2021-06-12 23:49:38 +0900

    [ADD] 10718번 We love krii

---
+++ 10718/10718.rb
@@ -0,0 +1,3 @@
+for i in 0..1
+    puts "강한친구 대한육군"
+end
---
+++ 10718/10718_3.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<2) {
+            System.out.println("강한친구 대한육군");
+            i++;
+        }
+    }
+}
